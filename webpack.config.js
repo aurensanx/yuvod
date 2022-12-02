de12c4423e5eada09b1bb5b408484ebe62eebd5b
Alberto Aurensanz <aaurensanz@hiberus.com>
2022-12-03 00:10:34 +0100

    Add webpack-dev-server for better developer experience

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 const path = require('path')
 
 module.exports = {
-  'entry': './src/index.js',
-  'mode': 'development',
-  'output': {
-    'filename': 'bundle.js',
-    'path': path.resolve('dist'),
-    'publicPath': '/',
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
   },
-  'module': {
-    'rules': [
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist'),
+    publicPath: '/dist',
+  },
+  module: {
+    rules: [
       {
-        'exclude': /node_modules/,
-        'test': /\.(js|jsx)$/,
-        'use': 'babel-loader',
+        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
       }
     ],
   },
